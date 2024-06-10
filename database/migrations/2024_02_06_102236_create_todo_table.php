87618f575f67ac83e7018b553e8de7bd02e25ec0
Anubrata Chatterjee <anubrata@technowebs.in>
2024-06-10 23:33:52 +0530

    -- Todo migration updated

--- database/migrations/2024_02_06_102236_create_todo_table.php
+++ database/migrations/2024_02_06_102236_create_todo_table.php
@@ -4,26 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('todo', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description')->nullable();
+    public function up(): void {
+        Schema::create( 'todos', function ( Blueprint $table ) {
+            $table->string( 'id', 45 )->primary();
+            $table->string( 'title' );
+            $table->text( 'description' )->nullable();
+            $table->softDeletes();
             $table->timestamps();
-        });
+        } );
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('todo');
+    public function down(): void {
+        Schema::dropIfExists( 'todos' );
     }
 };
