0f7f5878cec007487476083c8abc1524dae46877
Anubrata Chatterjee <anubrata@technowebs.in>
2024-06-10 23:32:52 +0530

    -- Message file creted and defined

---
+++ resources/lang/en/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'todo' => [
+        'created_successfully'         => 'Todo created successfully',
+        'updated_successfully'         => 'Todo updated successfully',
+        'not_found'                    => 'Todo not found',
+        'deleted_successfully'         => 'Todo deleted successfully',
+        'retrieved_successfully'       => 'Todo retrieved successfully',
+        'todos_retrieved_successfully' => 'All todos retrieved successfully',
+    ],
+];
