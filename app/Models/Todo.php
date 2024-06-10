11ddcf3df09637d386ad5cdec093a20180d7f33b
Anubrata Chatterjee <anubrata@technowebs.in>
2024-06-10 23:33:39 +0530

    ---- Todo model updated

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,13 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
-class Todo extends Model
-{
-    use HasFactory;
+class Todo extends Model {
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
         'description',
     ];
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating( function ( $model ) {
+            if ( empty( $model->id ) ) {
+                $model->id = Uuid::uuid4()->toString();
+            }
+        } );
+    }
+
+    protected $dates = ['deleted_at'];
+
 }
