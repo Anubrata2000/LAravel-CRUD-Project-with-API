289533cc5adeebee52fbf450d0981c8c0bdf5a85
Anubrata Chatterjee <anubrata@technowebs.in>
2024-06-10 23:40:06 +0530

    -- Api made for crud functionalities

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware( 'auth:sanctum' )->get( '/user', function ( Request $request ) {
     return $request->user();
-});
+} );
+
+Route::middleware( 'api' )->group( function () {
+    Route::get( '/todos', [TodoController::class, 'index'] )->name( 'todos.index' );
+    Route::get( '/todos/{id}', [TodoController::class, 'show'] )->name( 'todos.show' );
+    Route::post( '/todos', [TodoController::class, 'store'] )->name( 'todos.store' );
+    Route::put( '/todos/{id}', [TodoController::class, 'update'] )->name( 'todos.update' );
+    Route::delete( '/todos/{id}', [TodoController::class, 'destroy'] )->name( 'todos.destroy' );
+} );
