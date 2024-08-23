1b0d00437925792c50a53e96516be2e93f429632
Anubrata Chatterjee <chatterjeeanubrata386@gmail.com>
2024-08-24 00:16:02 +0530

    --Login and logout api routes made

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +20,7 @@ Route::middleware( 'auth:sanctum' )->get( '/user', function ( Request $request )
     return $request->user();
 } );
 
-Route::middleware( 'api' )->group( function () {
+Route::middleware( 'auth:sanctum' )->group( function () {
     Route::get( '/todos', [TodoController::class, 'index'] )->name( 'todos.index' );
     Route::get( '/todos/{id}', [TodoController::class, 'show'] )->name( 'todos.show' );
     Route::post( '/todos', [TodoController::class, 'store'] )->name( 'todos.store' );
@@ -27,10 +28,16 @@ Route::middleware( 'api' )->group( function () {
     Route::delete( '/todos/{id}', [TodoController::class, 'destroy'] )->name( 'todos.destroy' );
 } );
 
-Route::middleware( 'auth:api' )->group( function () {
+Route::middleware( 'auth:sanctum' )->group( function () {
     Route::get( 'users', [UserController::class, 'index'] ); // Get a list of users (paginated)
-    Route::post( 'users', [UserController::class, 'store'] ); // Create a new user
     Route::get( 'users/{id}', [UserController::class, 'show'] ); // Get a specific user by ID
     Route::put( 'users/{id}', [UserController::class, 'update'] ); // Update a user by ID
     Route::delete( 'users/{id}', [UserController::class, 'destroy'] ); // Delete a user by ID
+} );
+
+Route::post( 'users', [UserController::class, 'store'] ); // Create a new user
+Route::post( '/login', [UserController::class, 'login'] );
+
+Route::middleware( 'auth:sanctum' )->group( function () {
+    Route::get( '/logout', [UserController::class, 'logout'] );
 } );
